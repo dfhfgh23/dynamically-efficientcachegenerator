ece900ded06f5c8a0b101490316d28b172c18248
dfhfgh23 <js1littelis@hotmail.com>
2024-05-10 07:36:01 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const currentDate = new Date();
-console.log(currentDate); // Fri 2024-04-05T09:17:00.000Z
+function zigzagLevelOrder(root) {
+  if (!root) return [];
+  const result = [];
+  let isReverse = false;
+  const queue = [root];
+  while (queue.length) {
+    const size = queue.length;
+    const level = [];
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      if (isReverse) {
+        level.unshift(node.val);
+      } else {
+        level.push(node.val);
+      }
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(level);
+    isReverse = !isReverse;
+  }
+  return result;
+}
